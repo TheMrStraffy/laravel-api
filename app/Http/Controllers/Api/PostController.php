c1311084f7ec78f9ec10f97f83e22ee260bda3d4
TheMrStraffy <carmelo.miuccio2@gmail.com>
2023-01-26 16:34:13 +0100

    [fixed] PostController $projects mile2

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(){
-
+        $projects = Project::all();
+        return response()->json(compact('projects'));
     }
 
 }
