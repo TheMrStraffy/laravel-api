b992c7d1d7401ec9289fd211eff4c56b0a11767a
TheMrStraffy <carmelo.miuccio2@gmail.com>
2023-01-26 18:27:57 +0100

    [added] styles&project&component with print in

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,13 +1,52 @@
 <script>
+
+import axios from 'axios';
+import ProjectCardVue from './components/ProjectCard.vue';
 export default {
-    name: 'App'
+  name:'App',
+  components:{
+    ProjectCardVue,
+  },
+  data(){
+    return {
+      baseUrl: 'http://127.0.0.1:8000/api/',
+      projects: [],
+      technologies: [],
+      types: []
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(this.baseUrl + 'posts')
+      .then(result =>{
+        this.projects = result.data.projects;
+        this.technologies = result.data.technologies;
+        this.types = result.data.types;
+        console.log(this.projects);
+        console.log(this.technologies);
+        console.log(this.types);
+      })
+    }
+  },
+  mounted(){
+    this.getApi();
+  }
 }
 </script>
+
+
 <template>
-<h1>Hellooo here VUE IS BACK!</h1>
+  <h1>Main App Here</h1>
+  <div class="container">
+
+  <div class="row mx-auto">
+
+  <project-card-vue :projectList="this.projects" :technologyList="this.technologies" :typeList="this.types" />
+  </div>
+  </div>
 </template>
 
 
-<style>
-
+<style lang="scss">
+@use '../scss/appVue.scss';
 </style>
---
+++ resources/js/components/ProjectCard.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: "ProjectCard",
+  props:{
+    projectList: Array,
+    technologyList: Array,
+    typeList: Array
+    },
+  data(){
+    return {
+      
+      }
+  }
+}
+</script>
+<template>
+  <div 
+  v-for="project in projectList" :key="project.id"
+  class="card me-4 mb-4" style="width: 18rem;">
+  <img class="card-img-top" :src="project.cover_image" :alt="project.name">
+  <div class="card-body">
+    <h5 class="card-title">{{project.name}}</h5>
+
+    <div class="text-danger">
+    <span v-for="type in typeList" :key="type.id">
+      {{type.id == project.type_id ? type.name : ''}}
+      </span>
+    </div>
+    <div class="bg-dark">
+      <span class="text-white">Technologies here</span>
+    </div>
+    <p class="card-text">{{project.summary}}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</template>
+
+
+<style>
+
+</style>
